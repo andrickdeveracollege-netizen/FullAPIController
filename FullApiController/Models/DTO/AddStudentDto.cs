356100bb1e146f3a7e7ad98ba058edb16c14f6e6
Andrick Steven De Vera <andrick.devera.college@ustangelicum.edu.ph>
2026-09-05 01:34:37 +0800

    Change the uniqueness of the student num

--- FullApiController/Models/DTO/AddStudentDto.cs
+++ FullApiController/Models/DTO/AddStudentDto.cs
@@ -4,7 +4,7 @@ namespace FullApiController.Models.DTO
 {
     public class AddStudentDto
     {
-        [RegularExpression(@"^\d{8}$", ErrorMessage = "Student number must be in the format YEAR ENROLLED & BIRTH YEAR.")]
+        [RegularExpression(@"^\d{4}-\d{4}$", ErrorMessage = "Student number must be in the format YYYY-####.")]
         public string StudentNumber { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
